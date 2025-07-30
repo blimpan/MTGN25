0368513f58bba74c05628f9e6b0d910ff577fd67
Badrum1337 <adam.haneling@gmail.com>
2025-07-30 14:23:41 +0200

    Added mobile component for pdf rendering

---
+++ src/app/components/MobilePDFViewer.tsx
@@ -0,0 +1,46 @@
+import LinearProgress from '@mui/material/LinearProgress';
+import React, { useEffect, useState } from 'react';
+import { Document, Page as ReactPdfPage, pdfjs } from 'react-pdf';
+
+const Page = React.forwardRef(({ pageNumber, width }: { pageNumber: number, width: number }, ref) => {
+    return (
+        <div ref={ref}>
+            <ReactPdfPage pageNumber={pageNumber} width={width} />
+        </div>
+    );
+});
+
+function MobilePDFViewer({ src }: { src: string }) {
+    const [numPages, setNumPages] = useState<number>(0);
+    const [width, setWidth] = useState<number>(window.innerWidth - 32);
+
+    // Mandatory in order to render the PDF correctly
+    pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+        'pdfjs-dist/build/pdf.worker.min.mjs',
+        import.meta.url
+    ).toString();
+
+    // Handles the resizing of the window, changes the size of the PDF pages accordingly
+    useEffect(() => {
+        const handleResize = () => setWidth(window.innerWidth - 32);
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    return (
+        <div style={{ overflowY: 'auto', maxHeight: '80vh' }}>
+            <Document
+                file={src}
+                loading={<LinearProgress />}
+                noData={<LinearProgress />}
+                onLoadSuccess={({ numPages }) => setNumPages(numPages)}
+            > {/* This is what actually handles the render of the pdf */}
+                {Array.from({ length: numPages }, (_, i) => (
+                    <Page key={i + 1} pageNumber={i + 1} width={width} /> // Each page of the pdf specified in src 
+                ))}
+            </Document>
+        </div >
+    );
+}
+
+export default MobilePDFViewer;
